bdafe08aa4744678983a3e4b38e4f176a26df1de
PratikMitkar <pratikmitkar12@gmail.com>
2024-08-09 00:34:38 +0530

    responcieve PdfViewer

--- src/pages/BookDetail.jsx
+++ src/pages/BookDetail.jsx
@@ -8,6 +8,7 @@ const BookDetail = () => {
   const { bookName } = useParams();
   const book = booksData.books.find(book => book.name === bookName);
   const [selectedUnit, setSelectedUnit] = useState(null);
+  const [showUnits, setShowUnits] = useState(false);
 
   if (!book) {
     return <div>Book not found</div>;
@@ -18,20 +19,25 @@ const BookDetail = () => {
       <div className={styles.unitListContainer}>
         <img src={`/gate_notes/books/${bookName}/cover.png`} alt={`${bookName} Cover`} className={styles.bookCover} />
         <h2 className={styles.bookTitle}>{bookName}</h2>
-        <div className={styles.unitList}>
-          {book.units.map((unit, index) => (
-            <div
-              key={index}
-              className={`${styles.unit} ${selectedUnit === unit ? styles.activeUnit : ''}`}
-              onClick={() => setSelectedUnit(unit)}
-            >
-              <h3>{unit}</h3>
-              <button onClick={() => setSelectedUnit(unit)} className={styles.openPdfBtn}>
-                Open PDF
-              </button>
-            </div>
-          ))}
+        <div className={styles.unitListToggle} onClick={() => setShowUnits(!showUnits)}>
+          {showUnits ? 'Hide Units' : 'Show Units'}
         </div>
+        {showUnits && (
+          <div className={styles.unitList}>
+            {book.units.map((unit, index) => (
+              <div
+                key={index}
+                className={`${styles.unit} ${selectedUnit === unit ? styles.activeUnit : ''}`}
+                onClick={() => setSelectedUnit(unit)}
+              >
+                <h3>{unit}</h3>
+                <button onClick={() => setSelectedUnit(unit)} className={styles.openPdfBtn}>
+                  Open PDF
+                </button>
+              </div>
+            ))}
+          </div>
+        )}
       </div>
       <div className={styles.pdfReaderContainer}>
         {selectedUnit ? (
